b90960d84b1e6a80467663a12285be83ea07ef34
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-08-10 18:35:08 +0100

    :tada:

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           GeistMono.variable,
         )}
       >
+        <span>a</span>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <TRPCReactProvider>{props.children}</TRPCReactProvider>
           <div className="absolute bottom-4 right-4">
