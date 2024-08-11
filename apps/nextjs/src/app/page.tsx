b55d48eb12cf2f536e330b3825a2e9d45255a394
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-08-11 14:29:11 +0100

    :tada:

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
 
+import { db } from "@acme/db/client";
+
 import { api, HydrateClient } from "~/trpc/server";
 import { AuthShowcase } from "./_components/auth-showcase";
 import Bootstrap from "./_components/bootstrap";
@@ -11,7 +13,9 @@ import {
 
 export const runtime = "edge";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const schools = await db.query.School.findMany();
+
   // You can await this here if you don't want to show Suspense fallback below
   void api.post.all.prefetch();
 
@@ -22,6 +26,11 @@ export default function HomePage() {
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-primary">T3</span> Turbo
           </h1>
+          <div className="">
+            {schools.map((s) => (
+              <div id={s.id}>{s.name}</div>
+            ))}
+          </div>
           <AuthShowcase />
           <Bootstrap />
           <CreatePostForm />
