58823c114ba62cbabbf8669273e4e3d694421134
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-08-20 21:55:47 +0100

    :tada:

--- apps/nextjs/src/app/api/bot/route.ts
+++ apps/nextjs/src/app/api/bot/route.ts
@@ -18,13 +18,14 @@ const bot = new Bot(token);
 console.log("HELLO MESSAGED!!!!");
 bot.on("message", async (ctx) => {
   console.log(`MSG: ${ctx.message.text}`);
-  await ctx.reply(ctx.message.text ?? "HELLO");
-  await db.insert(Blogs).values({
-    mediaType: "audio",
-    status: "as",
-    telegramId: "adsss",
-    title: "lorem",
-  });
+  await ctx.reply(JSON.stringify(ctx.message));
+  // await db.insert(Blogs).values({
+  //   mediaType: "audio",
+  //   status: "as",
+  //   telegramId: "adsss",
+  //   title: "lorem",
+  //   // au
+  // });
 });
 // bot.on('message')
 // bot.on("msg:audio", async (ctx) => {});
