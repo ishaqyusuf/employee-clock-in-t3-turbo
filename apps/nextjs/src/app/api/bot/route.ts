9f236e53e63c7f48449e0f1b108a2ffd44d32c8e
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-08-20 12:39:11 +0100

    :tada:

--- apps/nextjs/src/app/api/bot/route.ts
+++ apps/nextjs/src/app/api/bot/route.ts
@@ -1,5 +1,8 @@
 import { Bot, webhookCallback } from "grammy";
 
+import { db } from "@acme/db/client";
+import { Blogs } from "@acme/db/schema";
+
 import { env } from "~/env";
 
 export const dynamic = "force-dynamic";
@@ -12,9 +15,16 @@ if (!token)
   throw new Error("TELEGRAM_BOT_TOKEN environment variable not found.");
 
 const bot = new Bot(token);
+console.log("HELLO MESSAGED!!!!");
 bot.on("message", async (ctx) => {
   console.log(`MSG: ${ctx.message.text}`);
   await ctx.reply(ctx.message.text ?? "HELLO");
+  await db.insert(Blogs).values({
+    mediaType: "audio",
+    status: "as",
+    telegramId: "adsss",
+    title: "lorem",
+  });
 });
 // bot.on('message')
 // bot.on("msg:audio", async (ctx) => {});
