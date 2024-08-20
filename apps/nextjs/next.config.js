f8798227685b9e213e60483130864ff8a6ff804c
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-08-20 09:58:17 +0100

    :tada:

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -20,6 +20,9 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  experimental: {
+    serverComponentsExternalPackages: ["grammy"],
+  },
 };
 
 export default config;
