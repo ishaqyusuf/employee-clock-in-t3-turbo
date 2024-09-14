07a3ea001993a4e07ae6a0d83586da21d0a8b325
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-09-14 23:02:21 +0100

    :tada:

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -20,15 +20,16 @@ export default async function middleware(req: NextRequest) {
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   const __hostName = `.${env.APP_ROOT_DOMAIN}`;
   const hostname = req.headers.get("host"); //!.replace(__hostName, __hostName);
-  const isProd = env.NODE_ENV == "production";
   const searchParams = req.nextUrl.searchParams.toString();
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = `${url.pathname}${
     searchParams.length > 0 ? `?${searchParams}` : ""
   }`;
+  const isProd = env.NODE_ENV == "production";
+  const _url = `/${isProd ? "daarul-hadith" : hostname}${path}`;
+  if (isProd) return NextResponse.rewrite(new URL(url, req.url));
   if (
-    hostname === env.APP_ROOT_DOMAIN &&
-    isProd
+    hostname === env.APP_ROOT_DOMAIN
     // ||
     // hostname === env.NEXT_PUBLIC_ROOT_DOMAIN
   ) {
@@ -40,7 +41,6 @@ export default async function middleware(req: NextRequest) {
   // // console.log(">>>>>>>>>>>>>>>");
   // // rewrite everything else to `/[domain]/[slug] dynamic route
   // console.log(["+++TENANT+++", hostname, path]);
-  const _url = `/${isProd ? "daarul-hadith" : hostname}${path}`;
-  console.log({ hostname, path, _url });
-  return NextResponse.rewrite(new URL(url, req.url));
+  // console.log({ hostname, path, _url });
+  return NextResponse.rewrite(new URL(_url, req.url));
 }
